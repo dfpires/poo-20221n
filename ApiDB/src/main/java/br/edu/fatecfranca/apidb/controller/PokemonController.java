57c87537be3bd824678000ecff353bcd862484e0
Daniel Pires <dfpires@gmail.com>
2022-05-27 22:15:52 -0300

    Add files via upload

--- ApiDB/src/main/java/br/edu/fatecfranca/apidb/controller/PokemonController.java
+++ ApiDB/src/main/java/br/edu/fatecfranca/apidb/controller/PokemonController.java
@@ -3,6 +3,7 @@ package br.edu.fatecfranca.apidb.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ public class PokemonController {
 	
 	// método get para consultar os pokemons na rota /pokemon
 	@GetMapping("/pokemon")
+	@CrossOrigin(origins="*")
 	public List<Pokemon> get(){
 		// select * from pokemon
 		return injecao.findAll();
@@ -33,6 +35,7 @@ public class PokemonController {
 	
 	//método post para inserir um pokemon na rota /pokemon
 	@PostMapping("/pokemon")
+	@CrossOrigin(origins="*")
 	public Pokemon add(@RequestBody Pokemon pokemon) {
 		Pokemon novoPokemon =  injecao.save(pokemon);
 		return novoPokemon;
@@ -40,6 +43,7 @@ public class PokemonController {
 	
 	// método delete para remover um pokmeon na tora /pokemon
 	@DeleteMapping("/pokemon/{id}")
+	@CrossOrigin(origins="*")
 	public String remove(@PathVariable Long id) {
 		
 		try { // tenta remover
@@ -53,7 +57,8 @@ public class PokemonController {
 	
 	  //método post para inserir um pokemon na rota /pokemon
 		@PutMapping("/pokemon")
-		public Pokemon updates(@RequestBody Pokemon pokemon) {
+		@CrossOrigin(origins="*")
+		public Pokemon update(@RequestBody Pokemon pokemon) {
 			// pokemon terá id, portante o save atualiza
 			Pokemon alteradoPokemon =  injecao.save(pokemon);
 			return alteradoPokemon;
